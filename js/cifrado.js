524f5bae77ea1e946d7893d9691dddaf7d3bbc10
oti2005 <panchatienecuc4@email.com>
2024-08-16 19:58:38 -0500

    agregando botones

---
+++ js/cifrado.js
@@ -0,0 +1,33 @@
+function desifrado_cifrado(texto, desplazamiento,accion){
+    const abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultado = '';
+
+    for (let i=0;i < texto.length; i++){
+        const letraActual = texto[i].toUpperCase();
+        const indiceActual = abecedario.indexOf(letraActual);
+
+        if(indiceActual === -1){
+            resultado += letraActual;
+        }else{
+            let nuevoIndice
+            if(accion == 1){
+             nuevoIndice = (indiceActual + desplazamiento)%abecedario.length;
+        }else{ 
+             nuevoIndice = (indiceActual - desplazamiento)%abecedario.length;
+             if(nuevoIndice < 0){
+                nuevoIndice += abecedario.length;
+            }
+            }
+            resultado += abecedario[nuevoIndice];
+         
+        }
+    }
+    return resultado;
+}
+/*
+let textoOriginal = "ZGDS";
+let desplazamiento = 18;
+let textoCifrado = desifrado_cifrado(textoOriginal,desplazamiento,0);
+console.log(textoCifrado);*/
+
+export default desifrado_cifrado; 
---
+++ js/srcipt.js
@@ -0,0 +1,20 @@
+import desifrado_cifrado from "./cifrado.js";
+
+const mensaje=document.getElementById("mensaje");
+const desplazamiento=document.getElementById("desplazamiento");
+const boton_cifrar=document.getElementById("cifrar");
+const boton_descifrar=document.getElementById("descifrar");
+
+boton_cifrar.addEventListener("click",()=>{
+    if(mensaje.value == ""){
+        alert("ingresa tu mensaje secreto");
+    }else if(desplazamiento.value ==""){
+        alert("ingresa tu clave secreta");
+    }else{
+        let texto ="";
+        let textoCifrado= desifrado_cifrado(mensaje.value,des.value,1);
+        texto += "El mensaje cifrado es:"
+        texto += textoCifrado;
+        document.getElementById("resultado").innerHTML=texto;
+    }
+})
